2c8ea35cdf2379f13af7aa32efed20403c07cd48
Matt Pryor <mkjpryor-stfc@users.noreply.github.com>
2017-03-23 09:19:28 +0000

    Bump version

--- jasmin_ldap/__init__.py
+++ jasmin_ldap/__init__.py
@@ -5,7 +5,7 @@ This is the main module for the JASMIN LDAP library.
 __author__ = "Matt Pryor"
 __copyright__ = "Copyright 2015 UK Science and Technology Facilities Council"
 
-__version__ = "0.2"
+__version__ = "0.3"
 
 from .core import *
 from .exceptions import *
